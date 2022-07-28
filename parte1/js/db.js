7f0ae99a17417acc0abb84317f99b4d1f4898fdf
Artur Comoli <artur.comoli@gmail.com>
2022-07-28 13:19:16 -0300

    feat: add main section styles

---
+++ parte1/js/db.js
@@ -0,0 +1,60 @@
+const db = [
+  {
+    img: "./assets/vitrine1.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "",
+    parcela: "",
+  },
+  {
+    img: "./assets/vitrine2.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "50% OFF",
+    parcela: "99x de R$ 99.99",
+  },
+  {
+    img: "./assets/vitrine3.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "",
+    parcela: "",
+  },
+  {
+    img: "./assets/vitrine4.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "NOVO",
+    parcela: "99x de R$ 99.99",
+  },
+  {
+    img: "./assets/vitrine1.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "",
+    parcela: "",
+  },
+  {
+    img: "./assets/vitrine2.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "50% OFF",
+    parcela: "99x de R$ 99.99",
+  },
+  {
+    img: "./assets/vitrine3.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "",
+    parcela: "",
+  },
+  {
+    img: "./assets/vitrine4.jpeg",
+    titulo: "Lorem ipsum dolor summit",
+    preco: 99.99,
+    status: "NOVO",
+    parcela: "99x de R$ 99.99",
+  },
+];
+
+export default db;
--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,50 @@
-/* Coloque o JS neste arquivo */
+import db from "./db.js";
+
+class GenerateList {
+  static ulNovidades = document.querySelector(".vitrine__novidades__lista");
+  static ulDestaques = document.querySelector(".vitrine__destaques__lista");
+
+  static card(img, titulo, preco, status, parcela) {
+    const li = document.createElement("li");
+    li.className = "vitrine__produto";
+    li.innerHTML = `
+    <a href="#">
+        <div class="vitrine__link">
+          <h3 class="vitrine__produto-comprar">COMPRA RÁPIDA</h3>
+          ${
+            status && `<span class="vitrine__produto-desconto">${status}</span>`
+          }
+          <img src="${img}" alt="" />
+        </div>
+    </a>
+    <h4>${titulo}</h4>
+    <span class="vitrine__produto__preco">R$ ${preco}</span>
+    ${
+      parcela
+        ? `<span>${parcela}</span>`
+        : `<span style="color: transparent">a</span>`
+    }`;
+
+    console.log(li);
+    return li;
+  }
+
+  static novidadesList() {
+    db.map(({ img, titulo, preco, status, parcela }) => {
+      const produto = GenerateList.card(img, titulo, preco, status, parcela);
+      console.log(produto);
+      return GenerateList.ulNovidades.appendChild(produto);
+    });
+  }
+
+  static destaquesList() {
+    db.map(({ img, titulo, preco, status, parcela }) => {
+      const produto = GenerateList.card(img, titulo, preco, status, parcela);
+      console.log(produto);
+      return GenerateList.ulDestaques.appendChild(produto);
+    });
+  }
+}
+
+GenerateList.novidadesList();
+GenerateList.destaquesList();
